e4f5b53a94ee57b1308f56fb3ac31ea81d1b6cd0
Gabriela Sosa <gsosa2703@gmail.com>
2020-07-07 21:41:17 -0300

    Event Loop

---
+++ conceptos/monohilo.js
@@ -0,0 +1,21 @@
+/*
+Lo primero que pasa aqui es que se abre un proceso de NOde, 
+que interpreta todo el archuvo y lo convierte en codigo maquina y una vez esto se ejecuta
+luego se cierra el proceso
+
+Es MUY MUY MUY importante de nuestro codigo, hay que comprobar todo las funciones ya que si algo falla 
+se para todo el programa 
+
+*/
+console.log('Hola mundo')
+
+let  i= 0
+setInterval(() => {
+    console.log(i)
+    i++
+    if(i === 5){
+        var a = 3 + z
+    }
+}, 1000)
+
+console.log('despues')
